5875c94215bf3afffae6a581bb1a778e9865cef3
James Lyne <jim+github@not-null.co.uk>
2021-07-07 22:16:13 +0100

    Migrate to gradle, java 16

--- src/main/java/xyz/derkades/spawnx/Main.java
+++ src/main/java/xyz/derkades/spawnx/Main.java
@@ -12,57 +12,54 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	
+
 	@Override
-	public void onEnable(){
+	public void onEnable() {
 		super.getServer().getPluginManager().registerEvents(this, this);
 		super.saveDefaultConfig();
 	}
-	
+
 	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-		if (command.getName().equalsIgnoreCase("setspawn")){
-			if (sender instanceof Player){
-				Player player = (Player) sender;
-				if (player.hasPermission("spawnx.setspawn")){
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("setspawn")) {
+			if (sender instanceof Player player) {
+				if (player.hasPermission("spawnx.setspawn")) {
 					setSpawnLocation(player.getLocation());
 					player.sendMessage(ChatColor.DARK_AQUA + "The spawn location has been set!");
-					return true;
 				} else {
 					player.sendMessage(ChatColor.RED + "You do not have permission to execute this command.");
-					return true;
 				}
 			} else {
 				sender.sendMessage("You have to be a player in order to execute this command.");
-				return true;
 			}
-		} else if (command.getName().equalsIgnoreCase("spawn")){
-			if (sender instanceof Player){
-				Player player = (Player) sender;
-				if (player.hasPermission("spawnx.spawn")){
+
+			return true;
+		} else if (command.getName().equalsIgnoreCase("spawn")) {
+			if (sender instanceof Player player) {
+				if (player.hasPermission("spawnx.spawn")) {
 					player.teleport(getSpawnLocation());
-					return true;
 				} else {
 					player.sendMessage(ChatColor.RED + "You do not have permission to execute this command.");
-					return true;
 				}
 			} else {
 				sender.sendMessage("You have to be a player in order to execute this command.");
-				return true;
 			}
+
+			return true;
 		}
+
 		return false;
 	}
-	
+
 	@EventHandler
-	public void onJoin(PlayerJoinEvent event){
-		if (getConfig().getBoolean("teleport-on-join")){
+	public void onJoin(PlayerJoinEvent event) {
+		if (getConfig().getBoolean("teleport-on-join")) {
 			Player player = event.getPlayer();
 			player.teleport(getSpawnLocation());
 		}
 	}
-	
-	private Location getSpawnLocation(){
+
+	private Location getSpawnLocation() {
 		String worldName = getConfig().getString("world-name");
 		double x = getConfig().getDouble("x");
 		double y = getConfig().getDouble("y");
@@ -71,8 +68,8 @@ public class Main extends JavaPlugin implements Listener {
 		float yaw = (float) getConfig().getDouble("yaw");
 		return new Location(Bukkit.getWorld(worldName), x, y, z, yaw, pitch);
 	}
-	
-	private void setSpawnLocation(Location loc){
+
+	private void setSpawnLocation(Location loc) {
 		getConfig().set("world-name", loc.getWorld().getName());
 		getConfig().set("x", loc.getX());
 		getConfig().set("y", loc.getY());
