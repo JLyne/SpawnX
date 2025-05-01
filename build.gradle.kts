388eae695a40cfeb686579f854bfda731001b807
James Lyne <jim+github@not-null.co.uk>
2025-05-01 21:27:35 +0100

    Update gradle, remove clean dependency from compileJava

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,6 @@ paper {
 
 tasks {
     compileJava {
-        dependsOn(clean)
         options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
         options.encoding = "UTF-8"
     }
